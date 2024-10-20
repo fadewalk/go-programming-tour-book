a2a94e278c00febfde499f3bb4b97b0ea326aff1
元贞 <35691435+fadewalk@users.noreply.github.com>
2024-10-20 22:58:31 +0800

    feat: 初始化项目配置和全局变量
    - 新增 launch.json 文件，配置 VS Code 调试环境
    - 创建 global 包，定义全局配置变量
    - 实现配置文件加载和解析功能
    - 初始化数据库、应用和服务器相关配置
    - 更新 main 函数，加载配置并设置 Gin 模式

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/fadewalk/go-programming-tour-book/blog-service/global"
 	"github.com/fadewalk/go-programming-tour-book/blog-service/internal/routers"
+	"github.com/fadewalk/go-programming-tour-book/blog-service/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+    err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting error: %s", err)
+	}
+
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Server",&global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.WriteTimeout *= time.Second
+	global.ServerSetting.ReadTimeout *= time.Second
+	return nil
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 
 	s := &http.Server{
