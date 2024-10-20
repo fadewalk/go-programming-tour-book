3435b90e97b56abc97cb9edd3d2892d8b42a2ab3
元贞 <35691435+fadewalk@users.noreply.github.com>
2024-10-20 17:58:51 +0800

    refactor(blog-service): 重构路由定义并优化服务器配置
    - 调整路由定义，提高代码可读性和维护性
    - 在 main.go 中增加 HTTP 服务器配置，优化性能和安全性

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/fadewalk/go-programming-tour-book/blog-service/internal/routers"
-	
 )
 
 func main() {
 	router := routers.NewRouter()
 
 	s := &http.Server{
+		
 		Addr: ":8080",
 		Handler: router,
+		ReadTimeout: 30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
+	s.ListenAndServe()
 }
